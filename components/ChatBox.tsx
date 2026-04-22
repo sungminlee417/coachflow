3ee71b48e930204033f0824fa8a4bbf655d1a699
Ainsx <eorms9762@gmail.com>
2026-04-22 12:58:07 -0700

    WIP:dm:basic UI

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -8,6 +8,13 @@ interface Client {
   full_name: string;
   email: string;
 }
+interface Messages {
+  id: string;
+  conversation_id: string;
+  sender_id: string;
+  content: string;
+  created_at: string;
+}
 
 type ChatBoxProps = {
   clientId: string;
@@ -15,6 +22,7 @@ type ChatBoxProps = {
   open: boolean;
   onClose: () => void;
 };
+
 // get Current user
 // I need supabase client
 export default function ChatBox({
@@ -32,6 +40,14 @@ export default function ChatBox({
   const minimize = () => setIsMinimized(!isMinimized); // set it to true
   console.log("conversationId:", conversationId); // with state, never assign variable directly, use setter function
   // built in react hook that runs callback on mount
+  // TODO: message                creates state/ <> generic(typescript syntax) /[] array of messages
+  const [messages, setMessages] = useState<Messages[]>([]); //empty array because no messages are loaded.
+  const [messageText, setMessageText] = useState("");
+  // messages = loop through messages to display chat bubbles in UI
+  // setMessages(function) = call this after fetching from Supabase to store results
+  // flow
+  // fetch from Supabase -> get array of messages -> setMessages(data) -> messages now has data
+  // -> render them on screen.
   useEffect(() => {
     // run on component mount and/or dependency change
     const setupConversation = async () => {
@@ -66,18 +82,27 @@ export default function ChatBox({
 
         conversationId = newConversation.id;
       }
+      const fetchMessages = async () => {
+        const { data } = await supabase // wait here until supabase responds then continues.
+          .from("messages")
+          .select("*")
+          .eq("conversation_id", conversationId)
+          .order("created_at", { ascending: true });
+        setMessages(data || []);
+      };
       // save to state
       // renders (unmounted) state will go away if we unmount
       setConversationId(conversationId);
+      fetchMessages();
     };
     setupConversation();
   }, [clientId]); // dependency array (if this change, useEffect will run again)
   return open ? (
     <div
-      className={`fixed bottom-4 right-4 w-80 ${isMinimized ? "h-auto" : "h-96"} bg-white rounded-lg shadow-lg`}
+      className={`fixed bottom-4 right-4 w-80 z-50 ${isMinimized ? "h-auto" : "h-96"} bg-white rounded-lg shadow-lg`}
     >
       {/*header div: it needs name and close/minimize button*/}
-      <div className="justify-between flex">
+      <div className="justify-between flex border-b bg-gray-100">
         <span>{client.full_name}</span>
         <div className="flex">
           <button
@@ -93,8 +118,15 @@ export default function ChatBox({
             x
           </button>
         </div>
-        <div className="message">message</div>
       </div>
+      {!isMinimized && (
+        <input
+          value={messageText}
+          onChange={(e) => setMessageText(e.target.value)}
+          className="absolute bottom-0 left-0 right-0 p-1.5 w-full border rounded-full px-2 py-1"
+          placeholder="Aa"
+        ></input>
+      )}
     </div>
   ) : null;
   {
